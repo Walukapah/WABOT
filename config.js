0329206a35c040944ded020a61a4e542108c9926
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-01-20 15:15:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/900435c6d3157c98c3c88.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up මොකදවෙන්නේ?",
 };
