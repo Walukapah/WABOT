6b9f903f8d66f52e55cca8cd0074c73a4425edd1
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-03-24 10:27:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~SOhXmIJZ#-vpURKgKBLfDUhaXFp6gm96r0gsgR1c7esqqFdKABZc",
+SESSION_ID: process.env.SESSION_ID || "eEMzjQ4K#zLqgPhVovAq6U3JjWivvSQF6zftciyhzwT1NWsbeTDA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up 💫",
 };
