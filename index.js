be8ba98b0eaa1a127456c400b055b26585c76a95
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-03-24 10:35:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,9 +69,9 @@ require("./plugins/" + plugin);
 console.log('PLUGINS INSTALLED SUCCESSFUL ✅')
 console.log('SRI-BOT CONNECTED SUCCESSFUL ✅')
 
-let up = `> SRI-BOT CONNECTED SUCCESSFUL ✅\n\nPREFIX: ${prefix}`;
+//let up = `> SRI-BOT CONNECTED SUCCESSFUL ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+//conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
 
 }
 })
